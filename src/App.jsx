b42f737e3ef8132eb674fd0251124b23c52d0ed8
Sagar Bhoge <sagarbhoge2003@gmail.com>
2024-07-03 09:29:43 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
-import CarrerPilot from '../public/CarrerPilot.svg'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1>{CarrerPilot}</h1>
+      <h2>hello</h2>
     </>
   )
 }
